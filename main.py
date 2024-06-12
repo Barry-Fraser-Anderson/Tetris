aad7808137639ad4fd313c2ce100bfaf37f4cd2e
Barry Anderson <barryanderson60@outlook.com>
2024-06-12 14:56:09 +0100

    add event loop

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import pygame
+import sys
 
 pygame.init()
 
 WIDTH, HEIGHT = 300, 600
+FPS = 60
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Python Tetris")
+
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(FPS)
