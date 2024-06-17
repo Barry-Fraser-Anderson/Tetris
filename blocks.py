d4578dbc8f52fe1374d6d7ad788f696a8e9cc7a2
Barry Anderson <barryanderson60@outlook.com>
2024-06-17 16:28:49 +0100

    change block colours to be more authentic

--- blocks.py
+++ blocks.py
@@ -1,16 +1,16 @@
 from block import Block
 from position import Position
 
-class LBlock(Block):
+class IBlock(Block):
     def __init__(self):
         super().__init__(id=1)
         self.cells = {
-            0: [Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2)],
-            1: [Position(0, 1), Position(1, 1), Position(2, 1), Position(2, 2)],
-            2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 0)],
-            3: [Position(0, 0), Position(0, 1), Position(1, 1), Position(2, 1)],
+            0: [Position(1, 0), Position(1, 1), Position(1, 2), Position(1, 3)],
+            1: [Position(0, 2), Position(1, 2), Position(2, 2), Position(3, 2)],
+            2: [Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)],
+            3: [Position(0, 1), Position(1, 1), Position(2, 1), Position(3, 1)],
         }
-        self.move(0, 3)
+        self.move(-1, 3)
 
 
 class JBlock(Block):
@@ -25,16 +25,16 @@ class JBlock(Block):
         self.move(0, 3)
 
 
-class IBlock(Block):
+class LBlock(Block):
     def __init__(self):
         super().__init__(id=3)
         self.cells = {
-            0: [Position(1, 0), Position(1, 1), Position(1, 2), Position(1, 3)],
-            1: [Position(0, 2), Position(1, 2), Position(2, 2), Position(3, 2)],
-            2: [Position(2, 0), Position(2, 1), Position(2, 2), Position(2, 3)],
-            3: [Position(0, 1), Position(1, 1), Position(2, 1), Position(3, 1)],
+            0: [Position(0, 2), Position(1, 0), Position(1, 1), Position(1, 2)],
+            1: [Position(0, 1), Position(1, 1), Position(2, 1), Position(2, 2)],
+            2: [Position(1, 0), Position(1, 1), Position(1, 2), Position(2, 0)],
+            3: [Position(0, 0), Position(0, 1), Position(1, 1), Position(2, 1)],
         }
-        self.move(-1, 3)
+        self.move(0, 3)
 
 
 class OBlock(Block):
